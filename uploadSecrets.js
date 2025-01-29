fb4b784552d6ef30e8187139ccda4b45cfdd99b7
Zubin Pratap <zubin.pratap@gmail.com>
2025-01-29 20:51:09 +1100

    renamed upload.js. Minor refactors

--- uploadSecrets.js
+++ uploadSecrets.js
@@ -1,11 +1,8 @@
-const {
-  SecretsManager,
-  createGist,
-} = require("@chainlink/functions-toolkit");
+const { SecretsManager, createGist } = require("@chainlink/functions-toolkit");
 const ethers = require("ethers");
 require("@chainlink/env-enc").config();
 
-const makeRequestSepolia = async () => {
+const encryptAndUploadSecrets = async () => {
   // hardcoded for Avalanche Fuji
   const routerAddress = "0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0";
   const donId = "fun-avalanche-fuji-1";
@@ -15,20 +12,16 @@ const makeRequestSepolia = async () => {
 
   // Initialize ethers signer and provider to interact with the contracts onchain
   const privateKey = process.env.PRIVATE_KEY; // fetch PRIVATE_KEY
-  if (!privateKey)
-    throw new Error(
-      "private key not provided - check your environment variables"
-    );
+  if (!privateKey) throw new Error("private key not provided - check your environment variables");
 
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
   const wallet = new ethers.Wallet(privateKey);
   const signer = wallet.connect(provider); // create ethers signer for signing transactions
 
+  //////// ENCRYPT SECRETS////////
 
-  //////// MAKE REQUEST ////////
-
-  console.log("\nMake request...");
+  console.log("\nEncrypting Secrets...");
 
   // First encrypt secrets and create a gist
   const secretsManager = new SecretsManager({
@@ -38,30 +31,25 @@ const makeRequestSepolia = async () => {
   });
   await secretsManager.initialize();
 
-  // Encrypt secrets
   const encryptedSecretsObj = await secretsManager.encryptSecrets(secrets);
 
-  console.log(`Creating gist...`);
+  //////// CREATE GITHUB GIST ////////
+
+  console.log(`Creating gist to store encrypted secrets...`);
   const githubApiToken = process.env.GITHUB_API_TOKEN;
-  if (!githubApiToken)
-    throw new Error(
-      "githubApiToken not provided - check your environment variables"
-    );
+  if (!githubApiToken) throw new Error("githubApiToken not provided - check your environment variables");
 
   // Create a new GitHub Gist to store the encrypted secrets
-  const gistURL = await createGist(
-    githubApiToken,
-    JSON.stringify(encryptedSecretsObj)
-  );
+  const gistURL = await createGist(githubApiToken, JSON.stringify(encryptedSecretsObj));
+
+  //////// ENCRYPT GIST URL ////////
   console.log(`\n✅Gist created ${gistURL} . Encrypt the URLs..`);
-  const encryptedSecretsUrls = await secretsManager.encryptSecretsUrls([
-    gistURL,
-  ]);
+  const encryptedSecretsUrls = await secretsManager.encryptSecretsUrls([gistURL]);
 
   console.log(`\n✅Secrets encrypted. URLs: ${encryptedSecretsUrls}`);
 };
 
-makeRequestSepolia().catch((e) => {
+encryptAndUploadSecrets().catch(e => {
   console.error(e);
   process.exit(1);
 });
