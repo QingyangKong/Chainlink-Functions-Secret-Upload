6bb208a8820e99c64d09a171623d2ee5020d8126
qingyangkong <frank.kong@smartcontract.com>
2025-02-11 21:11:30 +0800

    add script to upload secret to DON

---
+++ uploadToDON.js
@@ -0,0 +1,77 @@
+const {
+  SecretsManager,
+  createGist,
+} = require("@chainlink/functions-toolkit");
+const ethers = require("ethers");
+require("@chainlink/env-enc").config();
+
+const makeRequestSepolia = async () => {
+  // hardcoded for Avalanche Fuji
+  const routerAddress = "0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0";
+  const donId = "fun-avalanche-fuji-1";
+  const rpcUrl = process.env.AVALANCHE_FUJI_RPC_URL; // fetch Sepolia RPC URL
+  
+  const gatewayUrls = [
+    "https://01.functions-gateway.testnet.chain.link/",
+    "https://02.functions-gateway.testnet.chain.link/",
+  ];
+  const slotIdNumber = 0;
+  const expirationTimeMinutes = 15;
+
+  const secrets = { apikey: process.env.SUPABASE_API_KEY };
+
+  // Initialize ethers signer and provider to interact with the contracts onchain
+  const privateKey = process.env.PRIVATE_KEY; // fetch PRIVATE_KEY
+  if (!privateKey)
+    throw new Error(
+      "private key not provided - check your environment variables"
+    );
+
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+
+  const wallet = new ethers.Wallet(privateKey);
+  const signer = wallet.connect(provider); // create ethers signer for signing transactions
+
+
+  //////// MAKE REQUEST ////////
+
+  console.log("\nMake request...");
+
+  // First encrypt secrets and create a gist
+  const secretsManager = new SecretsManager({
+    signer: signer,
+    functionsRouterAddress: routerAddress,
+    donId: donId,
+  });
+  await secretsManager.initialize();
+
+  // Encrypt secrets
+  const encryptedSecretsObj = await secretsManager.encryptSecrets(secrets);
+
+  console.log(
+    `Upload encrypted secret to gateways ${gatewayUrls}. slotId ${slotIdNumber}. Expiration in minutes: ${expirationTimeMinutes}`
+  );
+
+    // Upload secrets
+    const uploadResult = await secretsManager.uploadEncryptedSecretsToDON({
+        encryptedSecretsHexstring: encryptedSecretsObj.encryptedSecrets,
+        gatewayUrls: gatewayUrls,
+        slotId: slotIdNumber,
+        minutesUntilExpiration: expirationTimeMinutes,
+    });
+
+    if (!uploadResult.success)
+        throw new Error(`Encrypted secrets not uploaded to ${gatewayUrls}`);
+    
+    console.log(`\n✅ Secrets uploaded properly to gateways ${gatewayUrls}! Gateways response: `, uploadResult);
+    
+    const donHostedSecretsVersion = parseInt(uploadResult.version); // fetch the reference of the encrypted secrets
+
+    console.log(`donHostedSecretsVersion is ${donHostedSecretsVersion}`);
+    
+};
+
+makeRequestSepolia().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
